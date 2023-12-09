94db453af36b006e2de3007931987615368c91bc
Bthcorn <bowornthat.maize@gmail.com>
2023-12-09 15:00:57 +0700

    complete ex04

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -60,24 +60,55 @@
 //     let todos = [];
 //     let divs = document.getElementById("ft_list").children;
 //     for (let i = 0; i < divs.length; i++) {
-//         todos.push(divs[i].innerHTML);
-//     }
-//     setCookie("todos", JSON.stringify(todos), 1);
-// }
+    //         todos.push(divs[i].innerHTML);
+    //     }
+    //     setCookie("todos", JSON.stringify(todos), 1);
+    // }
 
 $(document).ready(function() {
     $("#btn").click(function() {
         var todo = prompt("Enter new todo");
         if (todo != null) {
             var div = $("<div></div>").text(todo);
-            div.click(deleteTodo);
+            // set id to todo
+            div.attr("id", todo);
+            // div.click(deleteTodo);
             $("#ft_list").prepend(div);
             let todos = [];
             let divs = $("#ft_list").children();
             for (let i = 0; i < divs.length; i++) {
                 todos.push(divs[i].innerHTML);
             }
+            let json = JSON.stringify(todos);
+            $.cookie("todos", json, {expires: 1 , path: '/'}); // Convert todos to JSON string
         }
 
     });
+
+    $("#ft_list").on("click", "div", function() {
+        if (confirm("Are you sure you want to delete this todo?")) {
+            $(this).remove();
+            let todos = [];
+            let divs = $("#ft_list").children();
+            for (let i = 0; i < divs.length; i++) {
+                todos.push(divs[i].innerHTML);
+            }
+            let json = JSON.stringify(todos);
+            $.cookie("todos", json, {expires: 1, path: '/'});
+        }
+
+    });
+
+    $(window).on("load", function () {
+        let todos = $.cookie("todos");
+        if (todos != null) { // Use != instead of !==
+            todos = JSON.parse(todos);
+            for (let i = 0; i < todos.length; i++) {
+                var div = $("<div></div>").text(todos[i]);
+                div.attr("id", todos[i]);
+                $("#ft_list").prepend(div);
+            }
+        }
+    });
+
 });
